bc1095e94538d0833d02a73b5c29092efd1c02bf
Nicholas Petrasek <nicholas.petrasek@gmail.com>
2022-09-20 08:53:08 +0200

    updated books interface

--- src/interfaces/books.interface.ts
+++ src/interfaces/books.interface.ts
@@ -1,10 +1,18 @@
 import { User } from './users.interface';
 
 export interface Book {
-  isbn: string;
+  id: string;
+  isbn10: string;
+  isbn13: string;
   title: string;
-  description: string;
+  titleLong: string;
   imageUrl: string;
   publisher: string;
+  publishingDate: Date;
+  language: String;
+  edition: String;
+  dimensions: String;
+  pages: number;
+  authors: string[];
   createdBy: User;
 }
